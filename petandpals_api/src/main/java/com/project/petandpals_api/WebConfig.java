78ffb5c705a11a8d341822a73375d35075cd276c
qxb3 <qxbthree@gmail.com>
2024-10-28 22:58:08 +0000

    fix backend

--- petandpals_api/src/main/java/com/project/petandpals_api/WebConfig.java
+++ petandpals_api/src/main/java/com/project/petandpals_api/WebConfig.java
@@ -10,10 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "https://awaited-caring-ghost.ngrok-free.app",
-                        "http://localhost:3000"
-                )
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
